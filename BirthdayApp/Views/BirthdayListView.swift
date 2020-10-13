805f42e836ec1035ae72d77179d378c64405dc57
DJDrama <putzzang@hotmail.com>
2020-10-13 12:32:38 +0900

    Initial Commit

---
+++ BirthdayApp/Views/BirthdayListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BirthdayApp
+//
+//  Created by jb on 2020/10/13.
+//
+
+import SwiftUI
+
+struct BirthDayListView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct BirthDayListView_Previews: PreviewProvider {
+    static var previews: some View {
+        BirthDayListView()
+    }
+}
